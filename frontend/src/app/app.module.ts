b67d113d2a9e4b929bdb9d078d05d39ca1bdf53e
Daniel Massoudyan <47738720+massoudyan1@users.noreply.github.com>
2021-09-17 09:50:21 +0200

    property card and list create

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
