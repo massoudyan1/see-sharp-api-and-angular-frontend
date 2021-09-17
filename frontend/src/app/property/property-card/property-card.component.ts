bce7b2af11f1125e6269c3b2492d5fa9e7300493
Daniel Massoudyan <47738720+massoudyan1@users.noreply.github.com>
2021-09-17 23:37:35 +0200

    changes

--- frontend/src/app/property/property-card/property-card.component.ts
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -1,12 +1,16 @@
 import { identifierModuleUrl } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.scss']
 })
+
+
 export class PropertyCardComponent implements OnInit {
+
 
   @Input() property_id: any;
   @Input() property_name: any;
--- frontend/src/app/property/property-list/property-list.component.ts
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  Property: any = [{
+  Properties: any = [{
     "Id": 1,
     "Name": "Kame House",
     "Type": "House",
